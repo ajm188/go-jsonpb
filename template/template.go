458406143eaf4596c2cd1391e135d1e929c7653c
Andrew Mason <amason@hey.com>
2020-09-16 08:38:41 -0400

    template whitespace is ridiculous

--- template/template.go
+++ template/template.go
@@ -1,14 +1,13 @@
 package template
 
-const jsonpbTemplate = `
-package {{ .Name }}
+const jsonpbTemplate = `package {{ .Name }}
 
 import "github.com/ajm188/go-jsonpb"
 
-{{ range .ProtoTypes }}
+{{ range .ProtoTypes -}}
 func (m *{{ .Type }}) MarshalJSON() ([]byte, error) {
 	return jsonpb.Marshal(m)
 }
 
-{{ end }}
+{{- end }}
 `
