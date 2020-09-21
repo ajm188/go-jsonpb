63df2bcc4123cc94c585403e72a03525a4c42edb
Andrew Mason <amason@hey.com>
2020-09-21 08:18:18 -0400

    Alias shadowed package name

--- jsonpb.go
+++ jsonpb.go
@@ -3,14 +3,14 @@ package jsonpb
 import (
 	"bytes"
 
-	"github.com/golang/protobuf/jsonpb"
+	protojson "github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
 )
 
 // Marshal uses protobuf/jsonpb to marshal a proto message into JSON.
 func Marshal(pb proto.Message) ([]byte, error) {
 	buf := bytes.Buffer{}
-	m := jsonpb.Marshaler{}
+	m := protojson.Marshaler{}
 
 	if err := m.Marshal(&buf, pb); err != nil {
 		return nil, err
